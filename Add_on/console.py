2fc32a20ade1b141ea69ca0426985a3b304b5ccf
YC-L <game0427game@gmail.com>
2021-04-29 15:47:42 +0100

    update console.py

--- Add_on/console.py
+++ Add_on/console.py
@@ -23,12 +23,15 @@ def console(screen_width, screen_height):
         def print(text, sep='',end='\n'):
             label = QLabel(str(text)+end)
             label.setStyleSheet('color:white;')
+            label.setMaximumHeight(15)
             formlayout.insertRow(formlayout.rowCount()-1,QLabel(),label)
+
         if text[-1:] == ':':
             spacing = '    '
             mainlabel.setText('..')
             lineEdit['space'] = True
             lineEdit['buffer'] = text +'\n'
+
         elif lineEdit['space']== True:
             if text.isspace():
                 mainlabel.setText('>>')
@@ -61,8 +64,10 @@ def console(screen_width, screen_height):
         .connect(lambda :scroll.verticalScrollBar().setValue(scroll.verticalScrollBar().maximum()))
 
     mainwidget = QWidget()
+
     scroll.setWidget(mainwidget)
     mainwidget.setStyleSheet('background-color:black;')
+
     layout = QVBoxLayout()
     layout.addWidget(scroll)
     scroll.setWidgetResizable(True)
@@ -70,18 +75,22 @@ def console(screen_width, screen_height):
     formlayout = QFormLayout(mainwidget)
     formlayout.setMargin(0)
     formlayout.setSpacing(0)
-    formlayout.setVerticalSpacing(-1)
+    formlayout.setVerticalSpacing(0)
+
     from sys import version,platform
     label = QLabel(str(version)+' on '+platform)
     label.setStyleSheet('color:white;')
     formlayout.addRow(QLabel(),label)
+
     line = QLineEdit()
     lineEdit = {'now':line,'space':False}
     line.setFrame(False)
     line.returnPressed.connect(lambda :execute(0))
     line.setStyleSheet('background-color:black; color:white;')
     #line.setFont(QtGui.QFont('Lucida Sans Typewriter', 10))
+
     mainlabel = QLabel('>>')
     mainlabel.setStyleSheet('color:white;')
     formlayout.addRow(mainlabel,line)
+
     return layout
